3063fb9537072469751ffe221f70d75543e5af97
Gerard Eklu <gerard_eklu@homedepot.com>
2019-10-19 19:36:49 -0400

    Adding new item to the budget controller

--- app.js
+++ app.js
@@ -1,6 +1,53 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
-  // Some code
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+  return {
+    addItem: function(type, desc, val) {
+      var newItem, ID;
+      ID = 0;
+      if (type === "exp") {
+        newItem = new Expense(ID, desc, val);
+      } else if (type === "inc") {
+        newItem = new Income(ID, desc, val);
+      }
+
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Push it into our data structure
+      data.allItems[type].push(newItem);
+
+      // return the new element
+      return newItem;
+    },
+    testing: function() {
+      console.log(data);
+    }
+  };
 })();
 
 // UI CONTROLLER
@@ -14,10 +61,9 @@ var UIContoller = (function() {
   return {
     getInput: function() {
       return {
-        type: document.querySelector(DOMstrings.inputType).value,
+        type: document.querySelector(DOMstrings.inputType).value, //Will be either inc or exp
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
-        // input: document.querySelector(DOMstrings.inputBtn).value
       };
     },
     getDOMstrings: function() {
@@ -41,9 +87,12 @@ var controller = (function(budgetCtrl, UICtrl) {
   };
 
   var ctrlAddItem = function() {
+    var input, newItem;
     // 1. Get the field input data
-    var input = UICtrl.getInput();
+    input = UICtrl.getInput();
     // 2. Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI.
